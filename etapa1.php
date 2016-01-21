0a4117d664e63b3f252191fb6d51de39f13abf85
Rogério Pereira <rogerio@groupsofter.com.br>
2016-01-21 17:38:23 -0200

    Etapa 1 OK

---
+++ etapa1.php
@@ -0,0 +1,65 @@
+<?php header('Content-type: text/html; charset=UTF-8'); ?>
+
+<h1>Desafio Programado SexLog Etapa 1 - Rogério Pereira rogerio@groupsofter.com.br</h1>
+
+<h2>Entendendo a ETAPA 1:</h2>
+
+<p>
+    A primeira etapa é muito simples: você deve criar um código PHP que encontre e apresente o 10.001º número primo. 
+    Lembre-se lá do colégio, o número 1 não é primo, certo?"
+</p>
+
+<p>
+    <strong>O número 1 não é primo.</strong> Porque para ser primo ele precisa de dois divisores distintos (1 e o 
+    próprio número), e 1 só é divisivel por ele mesmo.
+</p>
+
+<p>
+    Procurando 10001° numero primo, Aguarde.
+</p>
+
+<?php
+    ini_set('max_execution_time', 30000);   //Define o tempo maximo para nao dar problema com o tempo de execução
+
+    $primos         = 0;                    //Quantidade de numeros primos
+    $numero         = 1;                    //inicia em 1 para que na primeira vez que rode o while ja some para 2
+    $procuraProximo;
+
+    //Enquanto o array não tiver 10001 elementos
+    while($primos < 10001){
+        $numero++;  //Incrementa o numero a ser buscado no inicio para no final nao ter 1 a mais
+
+        $isPrimo = true;
+
+        //Percorre todos os numeros de 2 a n verificando se é divisivel por outros numeros
+        for($i=2; $i<$numero; $i++){
+            //Se for divisivel (resto da divisão = 0) então não é primo
+            if(($numero % $i) == 0) {
+                $isPrimo = false;
+                break;
+            }
+        }
+
+        //Se a variavel ainda for true não caiu na condição da linha 35 então é primo 
+        //Aumenta o contador de primos encontrados
+        if($isPrimo == true)
+            $primos++;
+    }
+?>
+
+<h2>Resposta</h2>
+
+<p>
+    O 10001° numero primo é o numero <strong><?= $numero ?></strong><br/>
+    
+    <a 
+        href='http://sexlog.com/prova_<?= $numero ?>' 
+        alt='Etapa 2' 
+        title='Etapa 2' 
+        target='_blank'
+    >
+        Acessa a etapa 2
+    </a>
+</p>
+
+//104743
---
+++ instruções.md
@@ -0,0 +1,13 @@
+O DESAFIO:
+Queremos saber quão sagaz você é e esse desafio tem 3 etapas! Aqui você encontrará somente a primeira... Para encontrar as outras 2 etapas utilize suas habilidades como programador. 
+ 
+Na última etapa, você encontrará o e-mail para qual deve enviar seu currículo juntamente com o código PHP que fez para chegar às soluções que obteve em cada uma das etapas :)
+
+Entendendo a ETAPA 1:
+A primeira etapa é muito simples: você deve criar um código PHP que encontre e apresente o 10.001º número primo. Lembre-se lá do colégio, o número 1 não é primo, certo?
+ 
+Encontrando os dados das etapas seguintes:
+O resultado da etapa 1 te levará à URL com as pistas da etapa 2:
+sexlog.com/prova_xxxxxxxx onde 'xxxxxxxx' é o resultado encontrado na etapa 1. O mesmo processo se dá na transição da etapa 2 pra 3.
+
+RESPOSTA ETAPA 1
